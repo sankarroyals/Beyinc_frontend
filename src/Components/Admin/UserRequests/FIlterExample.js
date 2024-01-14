dad1190767613377f69289aaff3babaeecb4c85a
sankar mukesh <ysankarmukesh@gmail.com>
2024-01-14 19:27:38 +0530

    filter name

--- src/Components/Admin/UserRequests/FIlterExample.js
+++ src/Components/Admin/UserRequests/FIlterExample.js
@@ -47,7 +47,7 @@ export default function MultipleSelectCheckmarks({ names, filterName, setFilters
   return (
     <div>
       <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+        <InputLabel id="demo-multiple-checkbox-label">{filterName}</InputLabel>
         <Select
           labelId="demo-multiple-checkbox-label"
           id="demo-multiple-checkbox"
