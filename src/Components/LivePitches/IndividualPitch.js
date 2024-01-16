c1312680e0b16835e6fb2523d884e1446a499118
sankar mukesh <ysankarmukesh@gmail.com>
2024-01-16 21:23:24 +0530

    Review added

--- src/Components/LivePitches/IndividualPitch.js
+++ src/Components/LivePitches/IndividualPitch.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+import { ApiServices } from '../../Services/ApiServices'
+import { useDispatch } from 'react-redux'
+import { setToast } from '../../redux/AuthReducers/AuthReducer'
+import { ToastColors } from '../Toast/ToastColors'
+import { format } from 'timeago.js'
+import ReviewStars from './ReviewStars'
 
 const IndividualPitch = () => {
-    const {pitchId} = useParams()
+    const [pitch, setpitch] = useState('')
+    const [averagereview, setAverageReview] = useState(0)
+    const { pitchId } = useParams()
+    const dispatch = useDispatch()
+    useEffect(() => {
+        if (pitchId) {
+            ApiServices.fetchSinglePitch({ pitchId: pitchId }).then(res => {
+                console.log(res.data);
+                setpitch(res.data)
+                if (res.data.review!==undefined && res.data.review?.length > 0) {
+                    let avgR = 0
+                    res.data.review?.map((rev) => {
+                        avgR += rev.review
+                    })
+                    setAverageReview(avgR / res.data.review.length)
+                }
+            }).catch(err => {
+                dispatch(
+                    setToast({
+                        message: "Error Occured",
+                        bgColor: ToastColors.failure,
+                        visibile: "yes",
+                    })
+                );
+            })
+        }
+    }, [pitchId])
   return (
-    <div>IndividualPitch</div>
+      <div className='individualPitchContainer'>
+          <div className='bgPitch'>
+              <img src="https://www.f-cdn.com/assets/main/en/assets/project-view/logged-out/header.jpg?image-optimizer=force&format=webply&width=1920" alt="" />
+          </div>
+          <div className='indiPitchDetailsContainer'>
+              <div className='indiPitchDetails'>
+                  <div className='indiPitchHeading'>{pitch?.heading}</div>
+                  <div>
+                      <div className='indiPitchDate'>Posted about {format(pitch?.createdAt)}</div>
+                  </div>
+                  <div className='indiPitchDesc'>{pitch?.description}</div>
+                  <div className='indiPitchId'>Pitch ID: {pitch?._id}</div>
+                  <div className='indiPitchId'>
+                      <ReviewStars avg={averagereview} />
+                  </div>
+              </div>
+          </div>
+    </div>
   )
 }
 
---
+++ src/Components/LivePitches/ReviewStars.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ReviewStars = ({ avg }) => {
+    const filledStars = Math.round(avg * 2) / 2; // Round to the nearest half star
+
+    const renderStars = () => {
+        const totalStars = 5;
+        const stars = [];
+
+        for (let i = 1; i <= totalStars; i++) {
+            let starClass = 'empty';
+
+            if (i <= filledStars) {
+                starClass = 'filled';
+            } else if (i - 0.5 === filledStars) {
+                starClass = 'half-filled';
+            }
+
+            stars.push(<span key={i} className={`star ${starClass}`}>&#9733;</span>);
+        }
+
+        return stars;
+    };
+
+    return (
+        <div className="star-rating">
+            {renderStars()}
+            <span className="average-rating">{avg.toFixed(1)}</span>
+        </div>
+    );
+};
+
+export default ReviewStars;
