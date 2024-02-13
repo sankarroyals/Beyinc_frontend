8efb13b0cc9d43e868909119e638e601107d4890
sankar mukesh <ysankarmukesh@gmail.com>
2024-02-13 12:57:19 +0530

    gjh

--- src/Components/AllUsers/individualUser.js
+++ src/Components/AllUsers/individualUser.js
@@ -105,7 +105,8 @@ const IndividualUser = () => {
   const sendReview = async () => {
     await ApiServices.addUserReview({
       userId: user._id,
-      review: { email: JSON.parse(localStorage.getItem('user')).accessToken.email, review: filledStars },
+      review: {
+        email: jwtDecode(JSON.parse(localStorage.getItem("user")).accessToken).email, review: filledStars },
     })
       .then((res) => {
         dispatch(
