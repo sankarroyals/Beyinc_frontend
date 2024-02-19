384dbfd1270e6d6c5a2c62fb151b1648d4e1b581
Madhav V Rajeev <75824625+GodOfCoding1@users.noreply.github.com>
2024-02-19 18:37:01 +0530

    finished admin pitches

--- src/Components/Admin/pitchDecider/AllPitches.js
+++ src/Components/Admin/pitchDecider/AllPitches.js
@@ -1,92 +1,148 @@
-import { useEffect, useState } from 'react';
-import { AdminServices } from '../../../Services/AdminServices';
-import '../UserRequests/UserRequest.css'
-import { ApiServices } from '../../../Services/ApiServices';
-import MultipleSelectCheckmarks from '../UserRequests/FIlterExample';
-import PitchCard from './PitchCard';
+import { useEffect, useState } from "react";
+import { AdminServices } from "../../../Services/AdminServices";
+import "../UserRequests/UserRequest.css";
+import { ApiServices } from "../../../Services/ApiServices";
+import MultipleSelectCheckmarks from "../UserRequests/FIlterExample";
+import PitchCard from "./PitchCard";
+import { FilterCheckBoxes } from "../../LoggedInPitches/Filters";
+import CachedIcon from "@mui/icons-material/Cached";
 
 export default function AllPitches() {
-    const [roles, setRoles] = useState([])
-    const [emails, setEmails] = useState([])
-    useEffect(() => {
-        ApiServices.getAllRoles().then((res) => {
-            const roles = []
-            res.data?.map(r => {
-                roles.push(r.role)
-            })
-            setRoles(roles)
-        })
-    }, [])
-    const [filters, setFilters] = useState({
-        role: [],
-        status: ['pending'],
-        email: []
-
-    })
-    const [data, setData] = useState([])
-    const [filteredData, setFilteredData] = useState([])
-    const getAllPitches = async () => {
-        await AdminServices.getAllPitches().then((res) => {
-            setData(res.data);
-            const emails = []
-            res.data?.map(r => {
-                emails.push(r.email)
-            })
-            setEmails(emails)
-        })
+  const [roles, setRoles] = useState([]);
+  const [emails, setEmails] = useState([]);
+  useEffect(() => {
+    ApiServices.getAllRoles().then((res) => {
+      const roles = [];
+      res.data?.map((r) => {
+        roles.push(r.role);
+      });
+      setRoles(roles);
+    });
+  }, []);
+  const [filters, setFilters] = useState({
+    role: [],
+    status: ["pending"],
+    email: [],
+  });
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const getAllPitches = async () => {
+    await AdminServices.getAllPitches().then((res) => {
+      setData(res.data);
+      const emails = [];
+      res.data?.map((r) => {
+        emails.push(r.email);
+      });
+      setEmails(emails);
+    });
+  };
+  useEffect(() => {
+    getAllPitches();
+  }, []);
+  useEffect(() => {
+    if (data.length > 0) {
+      filterUsers();
     }
-    useEffect(() => {
-        getAllPitches()
-    }, [])
-    useEffect(() => {
-        if (data.length > 0) {
-            filterUsers()
+  }, [data]);
+
+  const filterUsers = () => {
+    let filteredData = [...data];
+    if (Object.keys(filters).length > 0) {
+      Object.keys(filters).map((ob) => {
+        if (filters[ob].length > 0) {
+          filteredData = filteredData.filter((f) =>
+            filters[ob].includes(f[ob])
+          );
         }
-    }, [data])
-
-
-    const filterUsers = () => {
-
-        let filteredData = [...data]
-        if (Object.keys(filters).length > 0) {
-            Object.keys(filters).map((ob) => {
-                if (filters[ob].length > 0) {
-                    filteredData = filteredData.filter(f => filters[ob].includes(f[ob]))
-                }
-            })
-        }
-        setFilteredData(filteredData);
+      });
     }
-    return (
-        <>
-            <div className='usersFilterContainer'>
-                Filters:
-                <div style={{ display: 'flex', gap: '5px', flexWrap: 'wrap' }}>
-                    <div>
-                        <MultipleSelectCheckmarks names={roles} filterName='role' setFilters={setFilters} filters={filters} />
-                    </div>
-                    <div>
-                        <MultipleSelectCheckmarks names={['pending', 'approved', 'rejected']} filterName='status' filters={filters} setFilters={setFilters} />
-                    </div>
-                    <div>
-                        <MultipleSelectCheckmarks names={emails} filterName='email' setFilters={setFilters} filters={filters} />
-                    </div>
-                    <button style={{ width: '50px', height: '50px', marginTop: '8px' }} onClick={filterUsers}>
-                        <i className='fas fa-search'></i>
-                    </button>
-                </div>
-              
-                <div className='filteredUsers'>
-                    {filteredData?.length > 0 ?
-                        filteredData?.map((d) => (
-                            <PitchCard d={d} />
-                        ))
-                        : <div className="pitch-container">
-                        <img className="no-requests" src='/no-requests.png'/>
-                         <div style={{marginLeft : '30px'}}>No Requests Found !</div>
-                         </div>}
-                </div>
+    setFilteredData(filteredData);
+  };
+  const [isSpinning, setSpinning] = useState(false);
+  const handleReloadClick = () => {
+    setSpinning(true);
+    setFilters({
+      role: [],
+      status: ["pending"],
+      email: [],
+    });
+  };
+
+  useEffect(() => {
+    filterUsers();
+  }, [filters]);
+  return (
+    <>
+      <div className="usersWrapper">
+        <div style={{ height: "83vh" }} className="filterContainer">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <div className="filterHeader">Filter By:</div>
+            <div title="Reset filters">
+              <CachedIcon
+                style={{ cursor: "pointer" }}
+                className={isSpinning ? "spin" : ""}
+                onClick={handleReloadClick}
+              />
             </div>
-        </>
-    );
+          </div>
+          <div className="tagFilter">
+            <div className="filter-header">
+              <b>Status</b>
+            </div>
+
+            <FilterCheckBoxes
+              rawData={["pending", "approved", "rejected"]}
+              dataKey="status"
+              filters={filters}
+              setFilters={setFilters}
+            />
+          </div>
+          <hr />
+          <div className="tagFilter">
+            <div className="filter-header">
+              <b>Role</b>
+            </div>
+
+            <FilterCheckBoxes
+              setFilters={setFilters}
+              filters={filters}
+              rawData={roles}
+              dataKey="role"
+            />
+          </div>
+          <hr />
+          <div className="tagFilter">
+            <div className="filter-header">
+              <b>Email</b>
+            </div>
+
+            <FilterCheckBoxes
+              setFilters={setFilters}
+              filters={filters}
+              showSearch={true}
+              rawData={emails}
+              dataKey="email"
+            />
+          </div>
+        </div>
+
+        <div className="filteredUsers">
+          {filteredData?.length > 0 ? (
+            filteredData?.map((d) => <PitchCard d={d} />)
+          ) : (
+            <div className="pitch-container">
+              <img className="no-requests" src="/no-requests.png" />
+              <div style={{ marginLeft: "30px" }}>No Requests Found !</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Components/LoggedInPitches/LoggedInPitches.js
+++ src/Components/LoggedInPitches/LoggedInPitches.js
@@ -1,82 +1,127 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { ApiServices } from '../../Services/ApiServices'
-import { setToast } from '../../redux/AuthReducers/AuthReducer'
-import { ToastColors } from '../Toast/ToastColors'
-import MultipleSelectCheckmarks from '../Admin/UserRequests/FIlterExample'
-import PitchCard from '../Admin/pitchDecider/PitchCard'
-import LoggedInPitchCard from './LoggedPitchCard'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ApiServices } from "../../Services/ApiServices";
+import { setToast } from "../../redux/AuthReducers/AuthReducer";
+import { ToastColors } from "../Toast/ToastColors";
+import MultipleSelectCheckmarks from "../Admin/UserRequests/FIlterExample";
+import PitchCard from "../Admin/pitchDecider/PitchCard";
+import LoggedInPitchCard from "./LoggedPitchCard";
+import CachedIcon from "@mui/icons-material/Cached";
+import { FilterCheckBoxes } from "./Filters";
 const LoggedInPitches = () => {
-    const { email } = useSelector(state => state.auth.loginDetails)
-    const dispatch = useDispatch()
-    const [filteredData, setFilteredData] = useState([])
-    const [data, setdata] = useState([])
-    useEffect(() => {
-        ApiServices.getuserPitches({ email: email }).then(res => {
-            setdata(res.data)
-        }).catch(err => {
-            dispatch(
-                setToast({
-                    message: "Error Occured",
-                    bgColor: ToastColors.failure,
-                    visible: "yes",
-                })
-            );
-        })
-    }, [])
-    const [filters, setFilters] = useState({
-        status: [],
-        pitchRequiredStatus: []
-    })
-    useEffect(() => {
-        if (data.length > 0) {
-            filterUsers()
-        }
-    }, [data])
-
-
-    const filterUsers = () => {
-
-        let filteredData = [...data]
-        if (Object.keys(filters).length > 0) {
-            Object.keys(filters).map((ob) => {
-                if (filters[ob].length > 0) {
-                    filteredData = filteredData.filter(f => filters[ob].includes(f[ob]))
-                }
-            })
-        }
-        setFilteredData(filteredData);
+  const { email } = useSelector((state) => state.auth.loginDetails);
+  const dispatch = useDispatch();
+  const [filteredData, setFilteredData] = useState([]);
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    ApiServices.getuserPitches({ email: email })
+      .then((res) => {
+        setdata(res.data);
+      })
+      .catch((err) => {
+        dispatch(
+          setToast({
+            message: "Error Occured",
+            bgColor: ToastColors.failure,
+            visible: "yes",
+          })
+        );
+      });
+  }, []);
+  const [filters, setFilters] = useState({
+    status: ["pending"],
+    pitchRequiredStatus: [],
+  });
+  useEffect(() => {
+    if (data.length > 0) {
+      filterUsers();
     }
-    return (
-        <>
-            <div className='usersFilterContainer'>
-                Filters:
-                <div style={{ display: 'flex', gap: '5px', flexWrap: 'wrap' }}>
-                    <div>
-                        <MultipleSelectCheckmarks names={['pending', 'approved', 'rejected']} filterName='status' filters={filters} setFilters={setFilters} />
-                    </div>
-                    <div>
-                        <MultipleSelectCheckmarks names={['hide', 'show']} filterName='pitchRequiredStatus' filters={filters} setFilters={setFilters} />
-                    </div>
-                    <button style={{ width: '50px', height: '50px', marginTop: '8px' }} onClick={filterUsers}>
-                        <i className='fas fa-search'></i>
-                    </button>
-                </div>
+  }, [data]);
 
-                <div className='filteredUsers'>
-                    {filteredData?.length > 0 ?
-                        filteredData?.map((d) => (
-                            <LoggedInPitchCard d={d} />
-                        ))
-                        : <div className="pitch-container">
-                        <img className="no-requests" src='/no-requests.png'/>
-                         <div style={{marginLeft : '30px'}}>No Requests Found !</div>
-                         </div>}
-                         
-                </div>
-            </div>
-        </>
-    )
-}
+  const filterUsers = () => {
+    let filteredData = [...data];
+    if (Object.keys(filters).length > 0) {
+      Object.keys(filters).map((ob) => {
+        if (filters[ob].length > 0) {
+          filteredData = filteredData.filter((f) =>
+            filters[ob].includes(f[ob])
+          );
+        }
+      });
+    }
+    setFilteredData(filteredData);
+  };
 
-export default LoggedInPitches 
+  const [isSpinning, setSpinning] = useState(false);
+  const handleReloadClick = () => {
+    setSpinning(true);
+    setFilters({
+      status: ["pending"],
+      pitchRequiredStatus: [],
+    });
+  };
+
+  useEffect(() => {
+    filterUsers();
+  }, [filters]);
+  return (
+    <div className="usersWrapper">
+      <div style={{ height: "83vh" }} className="filterContainer">
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <div className="filterHeader">Filter By:</div>
+          <div title="Reset filters">
+            <CachedIcon
+              style={{ cursor: "pointer" }}
+              className={isSpinning ? "spin" : ""}
+              onClick={handleReloadClick}
+            />
+          </div>
+        </div>
+        <div className="tagFilter">
+          <div className="filter-header">
+            <b>Status</b>
+          </div>
+
+          <FilterCheckBoxes
+            rawData={["pending", "approved", "rejected"]}
+            dataKey="status"
+            filters={filters}
+            setFilters={setFilters}
+          />
+        </div>
+        <hr />{" "}
+        <div className="tagFilter">
+          <div className="filter-header">
+            <b>Pitch Hide/Show</b>
+          </div>
+
+          <FilterCheckBoxes
+            rawData={["hide", "show"]}
+            dataKey="pitchRequiredStatus"
+            filters={filters}
+            setFilters={setFilters}
+          />
+        </div>
+      </div>
+
+      <div className="filteredUsers">
+        {filteredData?.length > 0 ? (
+          filteredData?.map((d) => <LoggedInPitchCard d={d} />)
+        ) : (
+          <div className="pitch-container">
+            <img className="no-requests" src="/no-requests.png" />
+            <div style={{ marginLeft: "30px" }}>No Requests Found !</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default LoggedInPitches;
