150fa60b04de37083604fae786d2098ebb4b47c1
ShivaShankar <ss3177806@gmail.com>
2024-02-21 12:24:39 +0530

    styles

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -882,7 +882,7 @@ const Navbar = () => {
               </div>
 
               <div
-                style={{ display: "flex", gap: "2px", borderRadius: "10px" }}
+                style={{ display: "flex", gap: "2px", borderRadius: "10px", justifyContent: 'center', alignItems: 'center'}}
               >
                 <button
                   onClick={submit}
